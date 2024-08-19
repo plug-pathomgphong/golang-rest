2f2f59c8f53a7c4041b299bf0eb6c5cc7b987ed5
plug <plug185@gmail.com>
2024-08-19 14:51:20 +0700

    Add SQL query to fetch events and improve query to insert event

--- models/event.go
+++ models/event.go
@@ -15,8 +15,6 @@ type Event struct {
 	UserId      int
 }
 
-var events = []Event{}
-
 func (e Event) Save() error {
 	query := `
 	INSERT INTO events(name, description, location, dateTime, user_id)
@@ -41,6 +39,24 @@ func (e Event) Save() error {
 	return err
 }
 
-func GetAllEvents() []Event {
-	return events
+func GetAllEvents() ([]Event, error) {
+	query := `SELECT * FROM events`
+	rows, err := db.DB.Query(query)
+	if err != nil {
+		return nil, err
+	}
+
+	defer rows.Close()
+
+	var events []Event
+	for rows.Next() {
+		var event Event
+		err := rows.Scan(&event.Id, &event.Name, &event.Description, &event.Location, &event.DateTime, &event.UserId)
+		if err != nil {
+			return nil, err
+		}
+
+		events = append(events, event)
+	}
+	return events, nil
 }
